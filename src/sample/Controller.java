14f812f9b7f7425c51a0b223f354909a367b695e
Grishberg <grishberg@ya.ru>
2015-08-31 23:20:21 +0700

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private void onFormatClicked(ActionEvent event) {
+        System.out.println("Вы нажали кнопку Войти!");
+    }
+}
