5716c0b39b1788331fba97c0b1f475de09bdc95a
Grishberg <grishberg@ya.ru>
2015-09-01 10:50:14 +0700

    fixed parser, added regex-filter

--- src/com/grishberg/Parser.java
+++ src/com/grishberg/Parser.java
@@ -9,17 +9,35 @@ import java.util.Map;
  * Created by g on 01.09.15.
  */
 public class Parser {
-    public static String format(String input){
+    public static String format(String input) {
         String out = "";
-        try{
-            input = input.replaceAll("<b>([^<]*)</b>","");
+        input = normalizeString(input);
+        try {
+            String reg = "(\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{0,4}\\s*\\d{1,4}-\\d{1,4}\\/info.goodline\\.btv\\.dev D\\/Retrofit﹕\\s)";
+            input = input.replaceAll(reg, "");
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             Map<String, Object> javaRootMapObject = gson.fromJson(input, Map.class);
             out = gson.toJson(javaRootMapObject);
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return out;
+    }
+
+    private static String normalizeString(String in) {
+        int firstBracketPos = -1;
+        int lastBracletPos = -1;
+        for (int i = 0; i < in.length(); i++) {
+            if(firstBracketPos < 0 && in.charAt(i)=='{'){
+                firstBracketPos = i;
+            }
+            if (in.charAt(i)=='}'){
+                lastBracletPos = i+1;
+            }
+        }
+        if(firstBracketPos >= 0 && lastBracletPos > 0){
+            return in.substring(firstBracketPos, lastBracletPos).replace("\n","");
+        }
+        return in;
     }
 }
