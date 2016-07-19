3d516ab3267a2132a910db384ad04891b38154b2
grishberg <grishberg@gmail.com>
2016-07-19 18:31:57 +0700

    added jackson deserializer

--- src/com/grishberg/Parser.java
+++ src/com/grishberg/Parser.java
@@ -1,8 +1,9 @@
 package com.grishberg;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -10,7 +11,6 @@ import java.util.Map;
  */
 public class Parser {
     public static Map<String, Object> format(String input) {
-        String out = "";
         input = normalizeString(input);
         try {
             //String reg = "(\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{0,4}\\s*\\d{1,8}-\\d{1,8}\\/info.goodline\\.btv\\.dev D\\/Retrofit﹕\\s)";
@@ -20,39 +20,41 @@ public class Parser {
             input = input.replaceAll(reg, "");
             input = input.replaceAll(reg2, "");
 
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            Map<String, Object> javaRootMapObject = gson.fromJson(input, Map.class);
-            return javaRootMapObject;
+            ObjectMapper mapper = new ObjectMapper();
+
+            Map<String, Object> map = mapper.readValue(input, new TypeReference<Map<String, Object>>() {
+            });
+
+            return map;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static String prettyPrint(Map<String, Object> map){
-        String out = "";
+    public static String prettyPrint(Map<String, Object> map) {
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            out = gson.toJson(map);
-        } catch (Exception e) {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return out;
+        return null;
     }
 
     private static String normalizeString(String in) {
         int firstBracketPos = -1;
         int lastBracletPos = -1;
         for (int i = 0; i < in.length(); i++) {
-            if(firstBracketPos < 0 && in.charAt(i)=='{'){
+            if (firstBracketPos < 0 && in.charAt(i) == '{') {
                 firstBracketPos = i;
             }
-            if (in.charAt(i)=='}'){
-                lastBracletPos = i+1;
+            if (in.charAt(i) == '}') {
+                lastBracletPos = i + 1;
             }
         }
-        if(firstBracketPos >= 0 && lastBracletPos > 0){
-            return in.substring(firstBracketPos, lastBracletPos).replace("\n","");
+        if (firstBracketPos >= 0 && lastBracletPos > 0) {
+            return in.substring(firstBracketPos, lastBracletPos).replace("\n", "");
         }
         return in;
     }
