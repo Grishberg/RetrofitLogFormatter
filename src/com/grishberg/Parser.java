9a1d32a882f3c78675021a544db87383bb1147d5
grishberg <grishberg@gmail.com>
2016-07-19 17:51:25 +0700

    fixed bugshake logs formatting

--- src/com/grishberg/Parser.java
+++ src/com/grishberg/Parser.java
@@ -14,11 +14,12 @@ public class Parser {
         input = normalizeString(input);
         try {
             //String reg = "(\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{0,4}\\s*\\d{1,8}-\\d{1,8}\\/info.goodline\\.btv\\.dev D\\/Retrofit﹕\\s)";
-            String reg = "(\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{0,8}\\s*\\d{1,8}-\\d{1,8}\\/\\S* D\\/Retrofit﹕\\s)";
+            String reg = "(\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{0,8}\\s*\\d{1,8}-\\d{1,8}\\/\\S* D\\/Retrofit﹕\\s)|\\G";
+            //\d{2}.\d{2}.\d{4}\s\d{2}:\d{2}:\d{2}\.\d{3}\sD\/Retrofit:\s
+            String reg2 = "(\\n?\\d{2}.\\d{2}.\\d{4}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\sD\\/Retrofit:\\s)";
             input = input.replaceAll(reg, "");
+            input = input.replaceAll(reg2, "");
 
-//            reg = "(\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d{0,8}\\s*\\d{1,8}-\\d{1,8}\\/\\S* D\\/Retrofit﹕\\s)";
-//            input = input.replaceAll(reg, "");
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             Map<String, Object> javaRootMapObject = gson.fromJson(input, Map.class);
             return javaRootMapObject;
